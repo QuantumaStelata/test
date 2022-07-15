16fc8e0ba29ba7b4c1863f7d577abc8ab1ecbcd6
QuantumaStelata <quantumastelata@gmail.com>
2022-07-15 16:21:11 +0300

    Add tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from main.models import Term, Brand, Style
 
 # Create your tests here.
+
+
+class ApiTests(APITestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.term = Term.objects.first()
+        self.brand = Brand.objects.first()
+        self.style = Style.objects.first()
+
+    def test_get_terms(self):
+        response = self.client.get(reverse("api:term-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+
+    def test_get_brands(self):
+        response = self.client.get(reverse("api:brand-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_get_styles(self):
+        response = self.client.get(reverse("api:style-list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+
+    def test_search(self):
+        url = reverse("api:search") + f"?s={self.term.slug if self.term else ''}&b={self.brand.slug if self.brand else ''}&st={self.style.slug if self.style else ''}"
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertJSONEqual(response.content, {
+            "term_id": self.term.id if self.term else None,
+            "brand_id": self.brand.id if self.brand else None,
+            "style_id": self.style.id if self.style else None
+        })
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from http import HTTPStatus as status
 
 # Create your tests here.
+
+
+class MainTests(TestCase):
+    def test_main_page(self):
+        response = self.client.get(reverse("main:main"))
+
+        self.assertEqual(response.status_code, status.OK)
